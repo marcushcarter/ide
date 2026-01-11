633308735e0305109572a3aa5e9fbb2ec52adc40
marcushcarter <marcoose408@gmail.com>
2026-01-10 21:59:43 -0500

    .

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 700,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    win.loadFile(path.join(__dirname, '../public/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
