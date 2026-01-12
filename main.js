ae8a5cb35d9a007340fafb2538505f8e9a4e40c5
marcushcarter <marcoose408@gmail.com>
2026-01-12 18:14:48 -0500

    panels static

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ function createWindow() {
         width: 1280,
         height: 800,
         minWidth: 300,
-        minHeight: 500,
         frame: false,
         titleBarStyle: 'hidden',
         icon: path.join(__dirname, "assets/icons/icon.ico"),
